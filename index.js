71f6f3a0a312df47d254110a5ca008d93f3caea9
salesforcemarvin <mlacuna@salesforce.com>
2024-07-09 17:01:45 +0800

    initial checkin

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.post("/save", (req, res) => {
+  // Handle save request
+  res.sendStatus(200);
+});
+app.post("/publish", (req, res) => {
+  // Handle publish request
+  res.sendStatus(200);
+});
+app.post("/validate", (req, res) => {
+  // Handle validate request
+  res.sendStatus(200);
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
